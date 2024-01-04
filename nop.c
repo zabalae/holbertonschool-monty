2d8317d38e9e37f5b3d9342529225a9878a40283
Abdiel-88 <abdiel.f@outlook.com>
2024-01-04 12:46:47 -0400

    fix betty fr

--- nop.c
+++ nop.c
@@ -5,11 +5,11 @@
  * @stack: Double pointer to the top of the stack
  * @line_number: Line number in the file where the nop command is
  *
- * 
+ *
  *
  */
 void nop(stack_t **stack, unsigned int line_number)
 {
-    (void)stack;
-    (void)line_number;
+	(void)stack;
+	(void)line_number;
 }
