d97753cba2e58849649219016e116ea214fc6c01
Abdiel-88 <abdiel.f@outlook.com>
2024-01-04 16:43:39 -0400

    task 4 add

--- swap.c
+++ swap.c
@@ -5,9 +5,9 @@
  * @stack: Double pointer to the top of the stack
  * @line_number: Line number in the file where the swap command is
  *
- * Description: Swaps the top two elements of the stack. If the stack does not
- *              contain at least two elements, the function prints an error
- *              message and exits with EXIT_FAILURE.
+ *
+ *
+ *
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
--- test.m
+++ test.m
@@ -1,4 +1,4 @@
 push 1
 push 2
-swap
+add
 pall
